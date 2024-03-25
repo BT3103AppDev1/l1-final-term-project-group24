5d399e86522316c256942038e6a70327396b0b9f
heezxeo <gusdhcj2004@gmail.com>
2024-03-25 16:11:10 +0800

    Calorie Intake Main Page

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from './components/Header.vue'
+import SideBar from './components/SideBar.vue'
+</script>
+
+<template>
+  <Header/>
+  <SideBar/>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: beige;
+}
+
+body {
+  padding:0;
+  margin: 0;
+}
+</style>
